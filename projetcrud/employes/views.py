10e10b6a68c97bb857fbadb8f034eee629e755bf
juste frank <justefrank9@gmail.com>
2026-01-20 03:08:41 +0000

    Ajout des vues CRUD Employe (create et read)

--- projetcrud/employes/views.py
+++ projetcrud/employes/views.py
@@ -1,3 +1,27 @@
 from django.shortcuts import render
+from django.views.generic import ListView, CreateView, UpdateView,DeleteView
+from .models import Employe
+from .forms import Employeform
+from django.urls import reverse_lazy
 
-# Create your views here.
+class EmployeListView(ListView):
+    model = Employe
+    template_name = 'index.html'
+    context_object_name = 'employes'
+
+# class pour l'ajout des employés
+class EmployeAjout(CreateView):
+
+    model = Employe
+    form_class = Employeform
+    template_name = 'index.html'
+    success_url = reverse_lazy('employe_list')
+    
+
+#class pour la modification
+class EmployEdit(UpdateView):
+
+    model = Employe
+    form_class = Employeform
+    template_name = 'index.html'
+    success_url = reverse_lazy('employe_list')
