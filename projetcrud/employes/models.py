63f2a682534f494ef453017729d897dc6f0a53c5
juste frank <justefrank9@gmail.com>
2026-01-20 03:05:05 +0000

    Ajout du modèle Employe

--- projetcrud/employes/models.py
+++ projetcrud/employes/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+#Class employés
+class Employe(models.Model):
+    nom = models.CharField(max_length=30)
+    prenom = models.CharField(max_length=50)
+    email = models.EmailField(unique=True)
+    adresse = models.TextField()
+    telephone = models.CharField(max_length=20)
+
+    def __str__(self):
+        return f"{self.prenom} {self.nom}"
