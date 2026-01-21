16b9d60580f3b56b921d799339e6f5feb4af5b13
juste frank <justefrank9@gmail.com>
2026-01-21 01:36:00 +0000

    implémentation de la route modifier et suppression

--- projetcrud/employes/urls.py
+++ projetcrud/employes/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import EmployeListView, EmployeAjout, EmployEdit
+from .views import EmployeListView, EmployeAjout, EmployEdit, EmployeDelete
 
 urlpatterns =[
     path('', EmployeListView.as_view(), name='employe_list'),
     path('ajout/', EmployeAjout.as_view(), name = 'employe_ajout'),
     path('edit/<int:pk>/', EmployEdit.as_view(), name='employe_edit'),
+    path('delete/<int:pk>/', EmployeDelete.as_view(), name='employe_delete')
 ]
